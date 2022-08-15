ba0dbf79b4f88d0aa04d934b1f478b6caa54edc9
Daniel <daniel@kylix.online>
2022-08-15 11:52:11 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (value) => {
+    return Math.abs (value - 42)
+}
+
+const distanceFromHqInFeet = (value) => {
+    return distanceFromHqInBlocks(value) * 264
+}
+
+const distanceTravelledInFeet = (start,destination) => {
+    const difference = destination-start
+return Math.abs (difference * 264)
+}
+
+const calculatesFarePrice = (start,destination) => {
+    const difference = destination-start
+    const distance = Math.abs (difference * 264)
+    if (distance <= 400) {
+        return 0
+    }
+    else if (distance > 400 && distance <=2000) {
+        return ((distance -400) * 2)/100
+    }
+    else if (distance > 2000 && distance <=2500) {
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
